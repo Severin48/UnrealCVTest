623d043c4865ca03d6e5f516d98d3d0ce78bd21d
Severin48 <severin2448@gmail.com>
2023-11-02 17:51:41 +0100

    Minimal and full example

--- full_example.py
+++ full_example.py
@@ -28,19 +28,36 @@ if __name__ == '__main__':
     client.connect()
 
     if not client.isconnected():
-        print('UnrealCV server is not running.')
+        print('UnrealCV server is not running. Run the game downloaded from http://unrealcv.github.io first.')
         sys.exit(-1)
 
+    res = client.request('vget /unrealcv/status')
+    print(res)
+
     try:
-        res = client.request('vget /camera/0/depth png')
+        # Get image
+        img_path = os.path.abspath(os.path.dirname(__file__)) + "/output/"
+        if not os.path.exists(img_path):
+            os.mkdir(img_path)
+        img_filename = img_path + "lit.png"
+        res = client.request(f'vget /camera/0/lit {img_filename}')
+        print('The image is saved to %s' % img_filename)
+
+        depth_filename = img_path + "depth.png"
+        res = client.request(f'vget /camera/0/depth {depth_filename}')
+        print('The image is saved to %s' % depth_filename)
+
+        # It is also possible to get the png directly without saving to a file
+        res = client.request('vget /camera/0/lit png')
         im = read_png(res)
+        print(im.shape)
 
         # Ground truth acquisition
         objects = client.request('vget /objects')
-        print("Objects:\t\t", objects)
+        print(objects)
         chair = objects.split(" ")[2]
         chair_loc = client.request(f'vget /object/{chair}/location')
-        print("Chair location:\t", chair_loc)
+        print("Chair location: ", chair_loc)
 
         plt.imshow(im)
         plt.show()
